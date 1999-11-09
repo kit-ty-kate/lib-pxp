972e2bf916c12bbcb29394b07ef1d6f901c3733d
gerd <gerd@dbe99aee-44db-0310-b2b3-d33182c8eb97>
1999-11-09 22:27:30 +0000

    The programs returns now an exit code of 1 if one of the XML files produces an error.
    git-svn-id: https://godirepo.camlcity.org/svn/lib-pxp/trunk@22 dbe99aee-44db-0310-b2b3-d33182c8eb97

--- examples/validate/validate.ml
+++ examples/validate/validate.ml
@@ -1,4 +1,4 @@
-(* $Id: validate.ml,v 1.2 1999/09/01 23:09:56 gerd Exp $
+(* $Id: validate.ml,v 1.3 1999/11/09 22:27:30 gerd Exp $
  * ----------------------------------------------------------------------
  *
  *)
@@ -13,6 +13,8 @@ let pr3 f a b c = try f a b c with Markup_types.At(where,what) -> print_endline 
 let pr4 f a b c d = try f a b c d  with Markup_types.At(where,what) -> print_endline where; print_endline (Printexc.to_string what); raise Not_found;;
 let pr5 f a b c d e = try f a b c d e  with Markup_types.At(where,what) -> print_endline where; print_endline (Printexc.to_string what); raise Not_found;;
 
+
+let error_happened = ref false;;
 
 let rec print_error e =
   match e with
@@ -40,6 +42,7 @@ let parse debug wf filename =
 	let s = default_config.warner # print_warnings in
 	if s <> "" then print_endline s;
 	default_config.warner # reset;
+	error_happened := true;
 	print_error e
 ;;
 
@@ -62,17 +65,21 @@ usage: validate [options] file ...
 
 List of options:";
   files := List.rev !files;
-  List.iter (parse !debug !wf) !files
+  List.iter (parse !debug !wf) !files;
 ;;
 
 
-main();;
-
+main();
+if !error_happened then exit(1);;
 
 (* ======================================================================
  * History:
  * 
  * $Log: validate.ml,v $
+ * Revision 1.3  1999/11/09 22:27:30  gerd
+ * 	The programs returns now an exit code of 1 if one of the
+ * XML files produces an error.
+ *
  * Revision 1.2  1999/09/01 23:09:56  gerd
  * 	Added the option -wf that switches to well-formedness checking
  * instead of validation.
