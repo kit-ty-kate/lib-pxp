9a01b53665535f2f3d2d81b3452dbee78cd66a83
gerd <gerd@dbe99aee-44db-0310-b2b3-d33182c8eb97>
2000-05-23 00:09:44 +0000

    The UTF-8 lexer set is no longer initialized here. It is done in the new module Pxp_utf8. Reason: You can link without UTF-8 support.
    git-svn-id: https://godirepo.camlcity.org/svn/lib-pxp/trunk@97 dbe99aee-44db-0310-b2b3-d33182c8eb97

--- src/pxp-engine/pxp_lexers.ml
+++ src/pxp-engine/pxp_lexers.ml
@@ -1,4 +1,4 @@
-(* $Id: pxp_lexers.ml,v 1.1 2000/05/20 20:30:50 gerd Exp $
+(* $Id: pxp_lexers.ml,v 1.2 2000/05/23 00:09:44 gerd Exp $
  * ----------------------------------------------------------------------
  *
  *)
@@ -7,49 +7,49 @@
 open Markup_types
 open Markup_lexer_types
 
+let lexer_set_iso88591 = 
+  { lex_encoding         = Enc_iso88591;
+    scan_document        = Pxp_lex_document_iso88591.scan_document;
+    scan_content         = Pxp_lex_content_iso88591.scan_content;
+    scan_within_tag      = Pxp_lex_within_tag_iso88591.scan_within_tag;
+    scan_document_type   = Pxp_lex_document_type_iso88591.
+			     scan_document_type;
+    scan_declaration     = Pxp_lex_declaration_iso88591.scan_declaration;
+    scan_content_comment  = Pxp_lex_misc_iso88591.scan_content_comment;
+    scan_decl_comment     = Pxp_lex_misc_iso88591.scan_decl_comment;
+    scan_document_comment = Pxp_lex_misc_iso88591.scan_document_comment;
+    scan_ignored_section = Pxp_lex_name_string_iso88591.
+                             scan_ignored_section;
+    scan_xml_pi          = Pxp_lex_misc_iso88591.scan_xml_pi;
+    scan_dtd_string      = Pxp_lex_dtd_string_iso88591.scan_dtd_string;
+    scan_content_string  = Pxp_lex_content_string_iso88591.
+			     scan_content_string;
+    scan_name_string     = Pxp_lex_name_string_iso88591.scan_name_string;
+    scan_only_xml_decl   = Pxp_lex_misc_iso88591.scan_only_xml_decl;
+    scan_for_crlf        = Pxp_lex_misc_iso88591.scan_for_crlf;
+  }
+;;
+
+
+let lexer_set_utf8 = ref None
+;;
+
+
+let init_utf8 ls =
+  lexer_set_utf8 := Some ls
+;;
+
+
 let get_lexer_set enc =
   match enc with
-      Enc_iso88591 ->
-	{ lex_encoding         = Enc_iso88591;
-	  scan_document        = Pxp_lex_document_iso88591.scan_document;
-	  scan_content         = Pxp_lex_content_iso88591.scan_content;
-	  scan_within_tag      = Pxp_lex_within_tag_iso88591.scan_within_tag;
-	  scan_document_type   = Pxp_lex_document_type_iso88591.
-				   scan_document_type;
-	  scan_declaration     = Pxp_lex_declaration_iso88591.scan_declaration;
-	  scan_content_comment  = Pxp_lex_misc_iso88591.scan_content_comment;
-          scan_decl_comment     = Pxp_lex_misc_iso88591.scan_decl_comment;
-          scan_document_comment = Pxp_lex_misc_iso88591.scan_document_comment;
-	  scan_ignored_section = Pxp_lex_name_string_iso88591.
-                                   scan_ignored_section;
-	  scan_xml_pi          = Pxp_lex_misc_iso88591.scan_xml_pi;
-	  scan_dtd_string      = Pxp_lex_dtd_string_iso88591.scan_dtd_string;
-	  scan_content_string  = Pxp_lex_content_string_iso88591.
-				   scan_content_string;
-	  scan_name_string     = Pxp_lex_name_string_iso88591.scan_name_string;
-	  scan_only_xml_decl   = Pxp_lex_misc_iso88591.scan_only_xml_decl;
-	  scan_for_crlf        = Pxp_lex_misc_iso88591.scan_for_crlf;
-	}
+      Enc_iso88591 -> lexer_set_iso88591
     | Enc_utf8 ->
-	{ lex_encoding         = Enc_utf8;
-	  scan_document        = Pxp_lex_document_utf8.scan_document;
-	  scan_content         = Pxp_lex_content_utf8.scan_content;
-	  scan_within_tag      = Pxp_lex_within_tag_utf8.scan_within_tag;
-	  scan_document_type   = Pxp_lex_document_type_utf8.
-				   scan_document_type;
-	  scan_declaration     = Pxp_lex_declaration_utf8.scan_declaration;
-	  scan_content_comment  = Pxp_lex_misc_utf8.scan_content_comment;
-          scan_decl_comment     = Pxp_lex_misc_utf8.scan_decl_comment;
-          scan_document_comment = Pxp_lex_misc_utf8.scan_document_comment;
-	  scan_ignored_section = Pxp_lex_name_string_utf8.scan_ignored_section;
-	  scan_xml_pi          = Pxp_lex_misc_utf8.scan_xml_pi;
-	  scan_dtd_string      = Pxp_lex_dtd_string_utf8.scan_dtd_string;
-	  scan_content_string  = Pxp_lex_content_string_utf8.
-				   scan_content_string;
-	  scan_name_string     = Pxp_lex_name_string_utf8.scan_name_string;
-	  scan_only_xml_decl   = Pxp_lex_misc_utf8.scan_only_xml_decl;
-	  scan_for_crlf        = Pxp_lex_misc_utf8.scan_for_crlf;
-	}
+	( match !lexer_set_utf8 with
+	      None ->
+		failwith ("Pxp_lexers: UTF-8 lexers not initialized")
+	    | Some ls ->
+		ls
+	)
     | _ ->
 	failwith ("Pxp_lexers: This type of internal encoding is not supported")
 ;;
@@ -58,6 +58,10 @@ let get_lexer_set enc =
  * History:
  * 
  * $Log: pxp_lexers.ml,v $
+ * Revision 1.2  2000/05/23 00:09:44  gerd
+ * 	The UTF-8 lexer set is no longer initialized here. It is done
+ * in the new module Pxp_utf8. Reason: You can link without UTF-8 support.
+ *
  * Revision 1.1  2000/05/20 20:30:50  gerd
  * 	Initial revision.
  *
--- src/pxp-engine/pxp_lexers.mli
+++ src/pxp-engine/pxp_lexers.mli
@@ -1,4 +1,4 @@
-(* $Id: pxp_lexers.mli,v 1.1 2000/05/20 20:30:50 gerd Exp $
+(* $Id: pxp_lexers.mli,v 1.2 2000/05/23 00:09:44 gerd Exp $
  * ----------------------------------------------------------------------
  *
  *)
@@ -12,10 +12,17 @@ val get_lexer_set : encoding -> lexer_set
    * encoding.
    *)
 
+val init_utf8 : lexer_set -> unit
+  (* Internally used. *)
+
 (* ======================================================================
  * History:
  * 
  * $Log: pxp_lexers.mli,v $
+ * Revision 1.2  2000/05/23 00:09:44  gerd
+ * 	The UTF-8 lexer set is no longer initialized here. It is done
+ * in the new module Pxp_utf8. Reason: You can link without UTF-8 support.
+ *
  * Revision 1.1  2000/05/20 20:30:50  gerd
  * 	Initial revision.
  *
