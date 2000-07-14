aa1dcfad9a44a73ca46c1a969e0e1050986039a3
gerd <gerd@dbe99aee-44db-0310-b2b3-d33182c8eb97>
2000-07-14 13:57:29 +0000

    Added the id_index feature.
    git-svn-id: https://godirepo.camlcity.org/svn/lib-pxp/trunk@170 dbe99aee-44db-0310-b2b3-d33182c8eb97

--- src/pxp-engine/pxp_yacc.mli
+++ src/pxp-engine/pxp_yacc.mli
@@ -1,4 +1,4 @@
-(* $Id: pxp_yacc.mli,v 1.4 2000/07/09 17:52:54 gerd Exp $
+(* $Id: pxp_yacc.mli,v 1.5 2000/07/14 13:57:29 gerd Exp $
  * ----------------------------------------------------------------------
  * PXP: The polymorphic XML parser for Objective Caml.
  * Copyright by Gerd Stolpmann. See LICENSE for details.
@@ -10,6 +10,39 @@
 open Pxp_types
 open Pxp_dtd
 open Pxp_document
+
+exception ID_not_unique
+
+class type [ 'ext ] index =
+object 
+  (* The type of indexes over the ID attributes of the elements. This type
+   * is the minimum requirement needed by the parser to create such an index.
+   *)
+  constraint 'ext = 'ext node #extension
+  method add : string -> 'ext node -> unit
+    (* Add the passed node to the index. If there is already an ID with
+     * the passed string value, the exception ID_not_unique should be
+     * raised. (But the index is free also to accept several identical IDs.)
+     *)
+  method find : string -> 'ext node
+    (* Finds the node with the passed ID value, or raises Not_found *)
+end
+;;
+
+
+class [ 'ext ] hash_index : 
+object 
+  (* This is a simple implementation of 'index' using a hash table. *)
+  constraint 'ext = 'ext node #extension
+  method add : string -> 'ext node -> unit
+    (* See above. *)
+  method find : string -> 'ext node
+    (* See above. *)
+  method index : (string, 'ext node) Hashtbl.t
+    (* Returns the hash table. *)
+end
+;;
+
 
 type config =
     { warner : collect_warnings;
@@ -79,6 +112,12 @@ type config =
         (* Whether the file name, the line and the column of the
 	 * beginning of elements are stored in the element nodes.
 	 * This option may be useful to generate helpful error messages.
+	 *)
+
+      idref_pass : bool;
+        (* Whether the parser does a second pass and checks that all
+	 * IDREF and IDREFS attributes contain valid references.
+	 * This option works only if an ID index is available.
 	 *)
 
       (* The following options are not implemented, or only for internal
@@ -176,20 +215,21 @@ val from_file :
 
 
 val default_config : config
-  (* - The resolver is able to read from files by name
-   * - Warnings are thrown away
-   * - Error message will contain line numbers
+  (* - Warnings are thrown away
+   * - Error messages will contain line numbers
    * - The internal encoding is ISO-8859-1
-   * - standalone declaration is checked
-   *)
+   * - The standalone declaration is checked
+   * - Element positions are stored
+   * - The IDREF pass is left out
+   *) 
 
 val default_extension : ('a node extension) as 'a
-  (* A "null" extension; an extension that does not extend the funtionality *)
+  (* A "null" extension; an extension that does not extend the functionality *)
 
 val default_spec : ('a node extension as 'a) spec
   (* Specifies that you do not want to use extensions. *)
 
-val parse_dtd_entity      : config -> source -> dtd
+val parse_dtd_entity : config -> source -> dtd
   (* Parse an entity containing a DTD (external subset), and return this DTD. *)
 
 val extract_dtd_from_document_entity : config -> source -> dtd
@@ -201,6 +241,7 @@ val extract_dtd_from_document_entity : config -> source -> dtd
 
 val parse_document_entity : 
   ?transform_dtd:(dtd -> dtd) ->
+  ?id_index:('ext index) ->
   config -> source -> 'ext spec -> 'ext document
   (* Parse a closed document, i.e. a document beginning with <!DOCTYPE...>,
    * and validate the contents of the document against the DTD contained
@@ -213,29 +254,34 @@ val parse_document_entity :
    *
    * If the optional argument ~transform_dtd is missing, the parser
    * behaves in the same way as if the identity were passed as ~transform_dtd.
+   *
+   * If the optional argument ~id_index is present, the parser adds
+   * any ID attribute to the passed index. An index is required to detect
+   * violations of the uniqueness of IDs.
    *)
 
-val parse_wfdocument_entity : config -> source -> 'ext spec -> 'ext document
+val parse_wfdocument_entity : 
+  config -> source -> 'ext spec -> 'ext document
   (* Parse a closed document (see parse_document_entity), but do not
    * validate it. Only checks on well-formedness are performed.
    *)
 
-val parse_content_entity  : config ->
-                            source ->
-			    dtd ->
-			    'ext spec ->
-			      'ext node
+val parse_content_entity  : 
+  ?id_index:('ext index) ->
+  config -> source -> dtd -> 'ext spec -> 'ext node
   (* Parse a file representing a well-formed fragment of a document. The
    * fragment must be a single element (i.e. something like <a>...</a>;
    * not a sequence like <a>...</a><b>...</b>). The element is validated
    * against the passed DTD, but it is not checked whether the element is
    * the root element specified in the DTD.
+   *
+   * If the optional argument ~id_index is present, the parser adds
+   * any ID attribute to the passed index. An index is required to detect
+   * violations of the uniqueness of IDs.
    *)
 
-val parse_wfcontent_entity : config ->
-                             source ->
- 	 		     'ext spec ->
-			       'ext node
+val parse_wfcontent_entity : 
+  config -> source -> 'ext spec -> 'ext node
   (* Parse a file representing a well-formed fragment of a document
    * (see parse_content_entity). The fragment is not validated, only
    * checked for well-formedness.
@@ -249,6 +295,9 @@ val parse_wfcontent_entity : config ->
  * History:
  *
  * $Log: pxp_yacc.mli,v $
+ * Revision 1.5  2000/07/14 13:57:29  gerd
+ * 	Added the id_index feature.
+ *
  * Revision 1.4  2000/07/09 17:52:54  gerd
  * 	New option store_element_positions.
  *
