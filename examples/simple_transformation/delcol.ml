eddd66f11aa0eb46c2fe9e787cc8af0f3ceae71d
gerd <gerd@dbe99aee-44db-0310-b2b3-d33182c8eb97>
2000-08-24 09:39:59 +0000

    Initial revision.
    git-svn-id: https://godirepo.camlcity.org/svn/lib-pxp/trunk@240 dbe99aee-44db-0310-b2b3-d33182c8eb97

---
+++ examples/simple_transformation/delcol.ml
@@ -0,0 +1,63 @@
+(* $Id: delcol.ml,v 1.1 2000/08/24 09:39:59 gerd Exp $
+ * ----------------------------------------------------------------------
+ *
+ *)
+
+(* Read a record-list, sort it, and print it as XML *)
+open Pxp_types;;
+open Pxp_document;;
+open Pxp_yacc;;
+
+let delcol col tree =
+  map_tree
+    ~pre:
+      (fun n -> 
+	 match n # node_type with
+	     T_element name when name = col ->
+	       raise Skip
+	   | _ -> n # orphaned_flat_clone)
+    tree
+;;
+
+
+let main() =
+  let column = ref "" in
+  Arg.parse
+      [ "-col", Arg.String (fun s -> column := s),
+	    " (last-name|first-name|phone)";
+      ]
+      (fun _ -> raise (Arg.Bad "Bad usage"))
+      "usage: sort [ options ]";
+  if !column = "" then (
+    prerr_endline "Column not specified!";
+    exit 1;
+  );
+  if not(List.mem !column ["last-name"; "first-name"; "phone"]) then (
+    prerr_endline ("Unknown column: " ^ !column);
+    exit 1
+  );
+  try
+    let dtd = parse_dtd_entity default_config (from_file "record.dtd") in
+    let tree = 
+      parse_content_entity default_config (from_channel stdin) dtd default_spec
+    in
+    print_endline "<?xml encoding='ISO-8859-1'?>";
+    (delcol !column tree) # write (Out_channel stdout) `Enc_iso88591
+  with
+      x ->
+	prerr_endline(string_of_exn x);
+	exit 1
+;;
+
+
+main();;
+
+(* ======================================================================
+ * History:
+ * 
+ * $Log: delcol.ml,v $
+ * Revision 1.1  2000/08/24 09:39:59  gerd
+ * 	Initial revision.
+ *
+ * 
+ *)
