dff51dd9c04b39dff5f23b98753ff3f622ad8d63
gerd <gerd@dbe99aee-44db-0310-b2b3-d33182c8eb97>
2001-06-07 22:39:20 +0000

    Pxp_codewriter now uses Pxp_marshal to generate the code. This simply reduces the complexity of the whole package a lot... 	A consequence is that Pxp_codewriter works for namespaces, because Pxp_marshal already does.
    git-svn-id: https://godirepo.camlcity.org/svn/lib-pxp/trunk@347 dbe99aee-44db-0310-b2b3-d33182c8eb97

--- src/pxp-engine/pxp_codewriter.ml
+++ src/pxp-engine/pxp_codewriter.ml
@@ -1,4 +1,4 @@
-(* $Id: pxp_codewriter.ml,v 1.8 2001/04/22 14:14:41 gerd Exp $
+(* $Id: pxp_codewriter.ml,v 1.9 2001/06/07 22:39:20 gerd Exp $
  * ----------------------------------------------------------------------
  * PXP: The polymorphic XML parser for Objective Caml.
  * Copyright by Gerd Stolpmann. See LICENSE for details.
@@ -8,469 +8,107 @@ open Pxp_document
 open Pxp_yacc
 open Pxp_dtd
 open Pxp_types
+open Pxp_marshal
 
-let write_expr_ext_id out extid =
-  match extid with
-      System s ->
-	output_string out ("(Pxp_types.System\"" ^ String.escaped s ^ "\")")
-    | Public(s,t) ->
-	output_string out ("(Pxp_types.Public(\"" ^ String.escaped s ^ 
-			   "\",\"" ^
-			   String.escaped t ^ "\"))")
-    | Anonymous ->
-	output_string out "Pxp_types.Anonymous"
-    | Private _ ->
-	failwith "Pxp_codewriter.write_expr_ext_id: Cannot represent a `Private' external ID"
-;;
-
-
-let rec write_expr_content_model out cm =
-  match cm with
-      Unspecified -> output_string out "Pxp_types.Unspecified"
-    | Empty       -> output_string out "Pxp_types.Empty"
-    | Any         -> output_string out "Pxp_types.Any"
-    | Mixed msl   -> output_string out "(Pxp_types.Mixed [";
-	             List.iter
-	               (fun ms ->
-			  write_expr_mixed_spec out ms;
-			  output_string out "; ";
-		       )
-		       msl;
-		     output_string out "])";
-    | Regexp re   -> output_string out "(Pxp_types.Regexp ";
-	             write_expr_regexp_spec out re;
-		     output_string out ")";
-
-and write_expr_mixed_spec out ms =
-  match ms with
-      MPCDATA  -> output_string out "Pxp_types.MPCDATA"
-    | MChild s -> output_string out ("(Pxp_types.MChild \"" ^
-				     String.escaped s ^ "\")")
-
-and write_expr_regexp_spec out re =
-  match re with
-      Optional re'  -> output_string out "(Pxp_types.Optional ";
-	               write_expr_regexp_spec out re';
-		       output_string out ")";
-    | Repeated re'  -> output_string out "(Pxp_types.Repeated ";
-	               write_expr_regexp_spec out re';
-		       output_string out ")";
-    | Repeated1 re' -> output_string out "(Pxp_types.Repeated1 ";
-	               write_expr_regexp_spec out re';
-		       output_string out ")";
-    | Alt rel       -> output_string out "(Pxp_types.Alt [";
-	               List.iter
-			 (fun re' ->
-			    write_expr_regexp_spec out re';
-			    output_string out "; ";
-			 )
-			 rel;
-		       output_string out "])";
-    | Seq rel       -> output_string out "(Pxp_types.Seq [";
-	               List.iter
-			 (fun re' ->
-			    write_expr_regexp_spec out re';
-			    output_string out "; ";
-			 )
-			 rel;
-		       output_string out "])";
-    | Child s       -> output_string out ("(Pxp_types.Child \"" ^ 
-					  String.escaped s ^ "\")")
-;;
-
-
-let write_expr_att_type out at =
-  match at with
-      A_cdata       -> output_string out "Pxp_types.A_cdata"
-    | A_id          -> output_string out "Pxp_types.A_id"
-    | A_idref       -> output_string out "Pxp_types.A_idref"
-    | A_idrefs      -> output_string out "Pxp_types.A_idrefs"
-    | A_entity      -> output_string out "Pxp_types.A_entity"
-    | A_entities    -> output_string out "Pxp_types.A_entities"
-    | A_nmtoken     -> output_string out "Pxp_types.A_nmtoken"
-    | A_nmtokens    -> output_string out "Pxp_types.A_nmtokens"
-    | A_notation sl -> output_string out "(Pxp_types.A_notation [";
-	               List.iter
-			 (fun s ->
-			    output_string out ("\"" ^ 
-					       String.escaped s ^ "\"; "))
-			 sl;
-		       output_string out "])";
-    | A_enum sl     -> output_string out "(Pxp_types.A_enum [";
-	               List.iter
-			 (fun s ->
-			    output_string out ("\"" ^ 
-					       String.escaped s ^ "\"; "))
-			 sl;
-		       output_string out "])";
-;;
-
-
-let write_expr_att_default out ad =
-  match ad with
-      D_required  -> output_string out "Pxp_types.D_required"
-    | D_implied   -> output_string out "Pxp_types.D_implied"
-    | D_default s -> output_string out ("(Pxp_types.D_default \"" ^
-					String.escaped s ^ "\")")
-    | D_fixed s   -> output_string out ("(Pxp_types.D_fixed \"" ^
-					String.escaped s ^ "\")")
-;;
-
-
-let write_expr_att_value out av =
-  match av with
-      Value s       -> output_string out ("(Pxp_types.Value \"" ^
-					  String.escaped s ^ "\")")
-    | Valuelist sl  -> output_string out ("(Pxp_types.Valuelist [");
-	               List.iter
-			 (fun s ->
-			    output_string out ("\"" ^ String.escaped s ^ 
-					       "\"; ")
-			 )
-			 sl;
-		       output_string out "])";
-    | Implied_value -> output_string out "Pxp_types.Implied_value"
-;;
-
-
-let ocaml_encoding enc =
-  match enc with
-      `Enc_utf8      -> "`Enc_utf8"
-    | `Enc_utf16     -> "`Enc_utf16"
-    | `Enc_utf16_le  -> "`Enc_utf16_le"
-    | `Enc_utf16_be  -> "`Enc_utf16_be"
-    | `Enc_iso88591  -> "`Enc_iso88591"
-;;
-
-
-let write_expr_new_pi out pi =
-  output_string out ("(new Pxp_dtd.proc_instruction \"" ^
-		     String.escaped(pi # target) ^ "\" \"" ^
-		     String.escaped(pi # value) ^ "\" " ^ 
-		     ocaml_encoding(pi # encoding) ^ ")")
-;;
-
-
-let write_expr_node_type out nt =
-  match nt with
-      T_data       -> output_string out "Pxp_document.T_data"
-    | T_element s  -> output_string out ("(Pxp_document.T_element \"" ^
-					 String.escaped s ^ "\")")
-    | T_super_root -> output_string out "Pxp_document.T_super_root"
-    | T_pinstr s   -> output_string out ("(Pxp_document.T_pinstr \"" ^
-					 String.escaped s ^ "\")")
-    | T_comment    -> output_string out "Pxp_document.T_comment"
-    | _            -> assert false
-;;
-
-
+(*
 let write_local_dtd out (dtd : dtd) =
-  (* Outputs "let mkdtd warner = ... in" to 'out' *)
-  output_string out "let mkdtd warner =\n";
-  output_string out ("let encoding = " ^ ocaml_encoding (dtd # encoding) ^ 
-                     " in\n");
-  output_string out "let dtdobj = new Pxp_dtd.dtd warner encoding in\n";
-  
-  (* Set the ID: *)
-  output_string out "dtdobj # set_id ";
-  begin match dtd # id with
-      None -> ()
-    | Some(External x) -> 
-	output_string out "(Pxp_types.External ";
-	write_expr_ext_id out x;
-	output_string out ");\n"
-    | Some(Derived x) ->  
-	output_string out "(Pxp_types.Derived ";
-	write_expr_ext_id out x;
-	output_string out ");\n"
-    | Some Internal ->   
-	output_string out "Pxp_types.Internal;\n";
-  end;
-
-  (* Set standalone declaration: *)
-  output_string out ("dtdobj # set_standalone_declaration " ^
-                     string_of_bool (dtd # standalone_declaration) ^ ";\n");
-
-  (* Add notations: *)
-  List.iter
-    (fun noname ->
-       let no = dtd # notation noname in
-       output_string out ("let no = new Pxp_dtd.dtd_notation \"" ^
-			  String.escaped noname ^ "\" ");
-       write_expr_ext_id out (no # ext_id);
-       output_string out " encoding in\n";
-       output_string out "dtdobj # add_notation no;\n";
-    )
-    (List.sort Pervasives.compare (dtd # notation_names));
-
-  (* Add unparsed entities: *)
-  List.iter
-    (fun enname ->
-       let en, _ = dtd # gen_entity enname in
-       if en # is_ndata then begin
-	 let ext_id = en # ext_id in
-	 let notation = en # notation in
-	 let encoding = en # encoding in
-	 output_string out ("let ndata = new Pxp_entity.ndata_entity \"" ^
-			    String.escaped enname ^ "\" ");
-	 write_expr_ext_id out ext_id;
-	 output_string out ("\"" ^ String.escaped notation ^ "\" " ^ 
-			    ocaml_encoding encoding ^ " in \n");
-	 output_string out "dtdobj # add_gen_entity (ndata :> Pxp_entity.entity) false;\n";
-       end;
-    )
-    (List.sort Pervasives.compare (dtd # gen_entity_names));
-
-
-  (* Add elements: *)
-  List.iter
-    (fun elname ->
-       (* Create the element 'el': *)
-       let el = dtd # element elname in
-       output_string out ("let el = new Pxp_dtd.dtd_element dtdobj \"" ^
-			  String.escaped elname ^ "\" in\n");
-       output_string out "let cm = ";
-       write_expr_content_model out (el # content_model);
-       output_string out " in\n";
-       output_string out "el # set_cm_and_extdecl cm false;\n";
-       (* Add attributes: *)
-       List.iter
-	 (fun attname ->
-	    let atttype, attdefault = el # attribute attname in
-	    output_string out ("el # add_attribute \"" ^ 
-			       String.escaped attname ^ "\" ");
-	    write_expr_att_type out atttype;
-	    output_string out " ";
-	    write_expr_att_default out attdefault;
-	    output_string out " false;\n";
-	 )
-	 (List.sort Pervasives.compare (el # attribute_names));
-
-       (* Allow arbitrary? *)
-       if el # arbitrary_allowed then
-         output_string out "el # allow_arbitrary;\n"
-       else
-         output_string out "el # disallow_arbitrary;\n";
-
-       (* Validate: *)
-       output_string out "el # validate;\n";
- 
-       (* Add the element 'el' to 'dtdobj': *)
-       output_string out "dtdobj # add_element el;\n";
-    )
-    (List.sort Pervasives.compare (dtd # element_names));
-
-  (* Add processing instructions: *)
-  List.iter
-    (fun target ->
-       let pilist = dtd # pinstr target in
-       List.iter
-	 (fun pi ->
-	    output_string out "let pi = ";
-	    write_expr_new_pi out pi;
-	    output_string out " in\n";
-	    output_string out "dtdobj # add_pinstr pi;\n";
-	 )
-	 pilist;
-    )
-    (List.sort Pervasives.compare (dtd # pinstr_names));
-
-  (* Set the name of the root element: *)
-  begin match dtd # root with
-      None -> ()
-    | Some rootname ->
-	output_string out ("dtdobj # set_root \"" ^
-			   String.escaped rootname ^ "\";\n")
-  end;
-
-  (* Special options: *)
-  if dtd # arbitrary_allowed then
-    output_string out "dtdobj # allow_arbitrary;\n"
-  else
-    output_string out "dtdobj # disallow_arbitrary;\n";
-
-  (* Return dtdobj: *)
-  output_string out "dtdobj in\n"
+  output_string out "let dtd = Pxp_yacc.parse_dtd_entity\n";
+  output_string out "  config\n";
+  output_string out "  (Pxp_yacc.from_string\n";
+  output_string out "     ~fixenc: `Enc_utf8\n";
+  output_string out "     \"";
+  let b = Buffer.create 100 in
+  dtd # write (Out_buffer b) `Enc_utf8 false;
+  output_string out (String.escaped (Buffer.contents b));
+  output_string out "\"";
+  output_string out "  ) in\n";
+  let root = dtd # root in
+  ( match root with
+	None -> ()
+      | Some r ->
+	  output_string out "dtd # set_root \"";
+	  output_string out (String.escaped r);
+	  output_string out "\"\n";
+  );
+  output_string out "dtd\n"
 ;;
+*)
 
 
 let rec write_local_subtree out n =
   (* Outputs the term generating the subtree *)
-  
-  output_string out "let nt = ";
-  write_expr_node_type out (n # node_type);
-  output_string out " in\n";
-
-  begin match n # node_type with
-      T_data ->
-	output_string out ("let t = Pxp_document.create_data_node spec dtd \"" ^
-			   String.escaped (n # data) ^ "\" in\n")
-    | T_element elname ->
-	let loc, line, col = n # position in
-	output_string out
-	  ("let pos = \"" ^ String.escaped loc ^ "\", " ^ 
-	   string_of_int line ^ ", " ^ 
-	   string_of_int col ^ " in\n");
-	output_string out 
-          ("let t = Pxp_document.create_element_node ~position:pos spec dtd \"" ^
-           String.escaped elname ^ "\" [ ");
-	List.iter
-	  (fun (name,value) ->
-	     begin match value with
-		 Value s -> 
-		   output_string out ("\"" ^ String.escaped name ^ "\", ");
-		   output_string out ("\"" ^ String.escaped s ^ "\"; ")
-	       | Valuelist sl ->
-		   output_string out ("\"" ^ String.escaped name ^ "\", ");
-		   output_string out ("\"" ^ 
-				      String.escaped (String.concat " " sl) ^ 
-				      "\"; ")
-	       | Implied_value ->
-		   ()
-	     end
-	  )
-	  (n # attributes);
-	output_string out " ] in\n";
-    | T_super_root ->
-	let loc, line, col = n # position in
-	output_string out
-	  ("let pos = \"" ^ String.escaped loc ^ "\", " ^ 
-	   string_of_int line ^ ", " ^ 
-	   string_of_int col ^ " in\n");
-	output_string out 
-          ("let t = Pxp_document.create_super_root_node ~position:pos spec dtd in\n")
-    | T_pinstr piname ->
-	let loc, line, col = n # position in
-	output_string out
-	  ("let pos = \"" ^ String.escaped loc ^ "\", " ^ 
-	   string_of_int line ^ ", " ^ 
-	   string_of_int col ^ " in\n");
-	output_string out "let pi = ";
-	write_expr_new_pi out (List.hd (n # pinstr piname));
-	output_string out " in\n";
-	output_string out 
-          ("let t = Pxp_document.create_pinstr_node ~position:pos spec dtd pi in\n")
-    | T_comment ->
-	let loc, line, col = n # position in
-	output_string out
-	  ("let pos = \"" ^ String.escaped loc ^ "\", " ^ 
-	   string_of_int line ^ ", " ^ 
-	   string_of_int col ^ " in\n");
-	output_string out "let comment = ";
-	( match n # comment with
-	      None   -> assert false
-	    | Some c -> output_string out ("\"" ^ String.escaped c ^ "\"")
-	);
-	output_string out " in\n";
-	output_string out 
-          ("let t = Pxp_document.create_comment_node ~position:pos spec dtd comment in\n")
-    | _ ->
-	assert false
-  end;
-
-  (* Add processing instructions: *)
-  begin match n # node_type with
-      T_pinstr _ ->
-	()
-    | _ ->
-	List.iter
-	  (fun target ->
-	     let pilist = n # pinstr target in
-	     List.iter
-	       (fun pi ->
-		  output_string out "let pi = ";
-		  write_expr_new_pi out pi;
-		  output_string out " in\n";
-		  output_string out "add_pinstr t pi;\n";
-	       )
-	       pilist;
-	  )
-	  (List.sort Pervasives.compare (n # pinstr_names));
-  end;
-       
-  (* Add the sub nodes: *)
-  n # iter_nodes
-    (fun n' ->
-       output_string out "add_node t (\n";
-       write_local_subtree out n';
-       output_string out ");\n";
-    );
-
-  (* Validate: *)
-  output_string out "local_validate t;\n";
-
-  (* Return: *)
-  output_string out "t\n"
+  output_string out "let cmds = ref [\n";
+  subtree_to_cmd_sequence 
+    (fun cmd ->
+       let s = Marshal.to_string cmd [] in
+       output_string out "\"";
+       output_string out (String.escaped s);
+       output_string out "\";\n";
+    )
+    n;
+  output_string out "] in\n";
+  output_string out "Pxp_marshal.subtree_from_cmd_sequence\n";
+  output_string out "  ?enable_namespace_processing\n";
+  output_string out "  (fun () -> match !cmds with\n";
+  output_string out "     cmd :: cmds' -> cmds := cmds'; Marshal.from_string cmd 0\n";
+  output_string out "   | []           -> assert false\n";
+  output_string out "  )\n";
+  output_string out "  dtd\n";
+  output_string out "  spec\n"
 ;;
 
 
 let write_local_document out (d : 'ext document) =
-  (* Outputs "let mkdoc warner spec = ... in" *)
-  
-  output_string out "let mkdoc warner spec =\n";
-  output_string out "let doc = new Pxp_document.document warner in\n";
-  output_string out ("doc # init_xml_version \"" ^
-		     String.escaped (d # xml_version) ^ "\";\n");
-  write_local_dtd out (d # dtd);
-  output_string out "let dtd = mkdtd warner in\n";
-  output_string out "let root = ";
-  write_local_subtree out (d # root);
-  output_string out " in\n";
-  output_string out "doc # init_root root;\n";
-
-  (* Add processing instructions: *)
-  List.iter
-    (fun target ->
-       let pilist = d # pinstr target in
-       List.iter
-	 (fun pi ->
-	    output_string out "let pi = ";
-	    write_expr_new_pi out pi;
-	    output_string out " in\n";
-	    output_string out "doc # add_pinstr pi;\n";
-	 )
-	 pilist;
+  output_string out "let cmds = ref [\n";
+  document_to_cmd_sequence 
+    (fun cmd ->
+       let s = Marshal.to_string cmd [] in
+       output_string out "\"";
+       output_string out (String.escaped s);
+       output_string out "\";\n";
     )
-    (List.sort Pervasives.compare (d # pinstr_names));
-  
-  (* Return the result: *)
-  output_string out "doc in\n"
-;;
-
-
-let write_helpers out =
-  output_string out "let add_node t n = (t : 'ext Pxp_document.node) # add_node (n : 'ext Pxp_document.node) in\n";
-  output_string out "let add_pinstr t pi = (t : 'ext Pxp_document.node) # add_pinstr (pi : Pxp_dtd.proc_instruction) in\n";
-  output_string out "let local_validate t = (t : 'ext Pxp_document.node) # local_validate ()in\n"
+    d;
+  output_string out "] in\n";
+  output_string out "Pxp_marshal.document_from_cmd_sequence\n";
+  output_string out "  (fun () -> match !cmds with\n";
+  output_string out "     cmd :: cmds' -> cmds := cmds'; Marshal.from_string cmd 0\n";
+  output_string out "   | []           -> assert false\n";
+  output_string out "  )\n";
+  output_string out "  config\n";
+  output_string out "  spec\n";
 ;;
 
 
 let write_document out d =
-  output_string out "let create_document warner spec =\n";
-  write_helpers out;
+  output_string out "let create_document config spec =\n";
   write_local_document out d;
-  output_string out "mkdoc warner spec;;\n"
+  output_string out ";;\n"
 ;;
 
 
 let write_dtd out dtd =
-  output_string out "let create_dtd warner =\n";
+  output_string out "let create_dtd config =\n";
   write_local_dtd out dtd;
-  output_string out "mkdtd warner;;\n"
+  output_string out ";;\n"
 ;;
 
 
 let write_subtree out t =
-  output_string out "let create_subtree dtd spec =\n";
-  write_helpers out;
+  output_string out "let create_subtree ?enable_namespace_processing dtd spec =\n";
   write_local_subtree out t;
-  output_string out "mktree dtd spec;;\n"
+  output_string out ";;\n"
 ;;
 
 (* ======================================================================
  * History:
  * 
  * $Log: pxp_codewriter.ml,v $
+ * Revision 1.9  2001/06/07 22:39:20  gerd
+ * 	Pxp_codewriter now uses Pxp_marshal to generate the code.
+ * This simply reduces the complexity of the whole package a lot...
+ * 	A consequence is that Pxp_codewriter works for namespaces,
+ * because Pxp_marshal already does.
+ *
  * Revision 1.8  2001/04/22 14:14:41  gerd
  * 	Updated to support private IDs.
  *
