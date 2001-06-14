795dfce367249d92d719a4002bbaa7a319caa40b
gerd <gerd@dbe99aee-44db-0310-b2b3-d33182c8eb97>
2001-06-14 15:47:39 +0000

    Initial revision.
    git-svn-id: https://godirepo.camlcity.org/svn/lib-pxp/trunk@372 dbe99aee-44db-0310-b2b3-d33182c8eb97

---
+++ src/pxp-lex-iso88591/pxp_lex_link_iso88591.ml
@@ -0,0 +1,45 @@
+(* $Id: pxp_lex_link_iso88591.ml,v 1.1 2001/06/14 15:47:39 gerd Exp $
+ * ----------------------------------------------------------------------
+ *
+ *)
+
+open Pxp_types
+open Pxp_lexer_types
+
+
+let lexer_set_iso88591 = 
+  { lex_encoding         = `Enc_iso88591;
+    scan_document        = Pxp_lex_document_iso88591.scan_document;
+    scan_content         = Pxp_lex_content_iso88591.scan_content;
+    scan_within_tag      = Pxp_lex_within_tag_iso88591.scan_within_tag;
+    scan_document_type   = Pxp_lex_document_type_iso88591.
+                             scan_document_type;
+    scan_declaration     = Pxp_lex_declaration_iso88591.scan_declaration;
+    scan_content_comment  = Pxp_lex_misc_iso88591.scan_content_comment;
+    scan_decl_comment     = Pxp_lex_misc_iso88591.scan_decl_comment;
+    scan_document_comment = Pxp_lex_misc_iso88591.scan_document_comment;
+    scan_ignored_section = Pxp_lex_name_string_iso88591.
+                             scan_ignored_section;
+    scan_xml_pi          = Pxp_lex_misc_iso88591.scan_xml_pi;
+    scan_dtd_string      = Pxp_lex_dtd_string_iso88591.scan_dtd_string;
+    scan_content_string  = Pxp_lex_content_string_iso88591.
+                             scan_content_string;
+    scan_name_string     = Pxp_lex_name_string_iso88591.scan_name_string;
+    scan_only_xml_decl   = Pxp_lex_misc_iso88591.scan_only_xml_decl;
+    scan_for_crlf        = Pxp_lex_misc_iso88591.scan_for_crlf;
+  }
+;;
+
+
+Pxp_lexers.init lexer_set_iso88591
+;;
+
+(* ======================================================================
+ * History:
+ * 
+ * $Log: pxp_lex_link_iso88591.ml,v $
+ * Revision 1.1  2001/06/14 15:47:39  gerd
+ * 	Initial revision.
+ *
+ * 
+ *)
