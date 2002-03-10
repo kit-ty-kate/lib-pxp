0a690379df0a9168d2822ccc0833a600ffcb7e0f
gerd <gerd@dbe99aee-44db-0310-b2b3-d33182c8eb97>
2002-03-10 23:44:40 +0000

    Initial revision.
    git-svn-id: https://godirepo.camlcity.org/svn/lib-pxp/trunk@480 dbe99aee-44db-0310-b2b3-d33182c8eb97

---
+++ rtests/codewriter/marshal_recode.ml
@@ -0,0 +1,56 @@
+(* $Id: marshal_recode.ml,v 1.1 2002/03/10 23:44:40 gerd Exp $
+ * ----------------------------------------------------------------------
+ *
+ *)
+
+open Pxp_yacc
+open Pxp_document
+open Pxp_types
+
+let conf =
+  { default_config with
+      enable_pinstr_nodes = true;
+      enable_super_root_node = true;
+      enable_comment_nodes = true;
+      encoding = `Enc_iso88591;
+  };;
+
+let conf' =
+  { conf with
+      encoding = `Enc_utf8;
+  };;
+
+let main() =
+  let doc = parse_document_entity
+	      conf
+	      (from_file "sample003.xml")
+	      default_spec in
+  let out1 = open_out "sample003.xml.out1" in
+  doc # write (`Out_channel out1) `Enc_utf8;
+  close_out out1;
+
+  let doc' = Pxp_marshal.relocate_document doc conf' default_spec in
+  let out2 = open_out "sample003.xml.out2" in
+  doc' # write (`Out_channel out2) `Enc_utf8;
+  close_out out2;
+
+  assert(Sys.command "cmp sample003.xml.out1 sample003.xml.out2" = 0);
+  
+  ()
+;;
+
+main();;
+
+
+
+
+
+(* ======================================================================
+ * History:
+ * 
+ * $Log: marshal_recode.ml,v $
+ * Revision 1.1  2002/03/10 23:44:40  gerd
+ * 	Initial revision.
+ *
+ * 
+ *)
