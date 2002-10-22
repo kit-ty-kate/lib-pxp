e34c834213a64c83f04c14308a5b85a69e3cdaf1
gerd <gerd@dbe99aee-44db-0310-b2b3-d33182c8eb97>
2002-10-22 14:22:54 +0000

    Corrected comment: pop_entity_until does NOT close the topmost entity.
    git-svn-id: https://godirepo.camlcity.org/svn/lib-pxp/trunk@586 dbe99aee-44db-0310-b2b3-d33182c8eb97

--- src/pxp-engine/pxp_entity_manager.ml
+++ src/pxp-engine/pxp_entity_manager.ml
@@ -1,4 +1,4 @@
-(* $Id: pxp_entity_manager.ml,v 1.1 2002/07/14 23:05:15 gerd Exp $
+(* $Id: pxp_entity_manager.ml,v 1.2 2002/10/22 14:22:54 gerd Exp $
  * ----------------------------------------------------------------------
  *
  *)
@@ -141,8 +141,8 @@ class entity_manager
 
     method pop_entity_until until_ent =
       (* pops entities from the stack and ensures that they are closed until
-       * [until_ent] is the top element. [until_ent] is the last entity
-       * being closed.
+       * [until_ent] is the top element. All entities that are taken from
+       * the stack are closed. [until_ent] is not closed.
        *)
       try
 	while current_entity <> until_ent do
@@ -160,6 +160,10 @@ class entity_manager
  * History:
  * 
  * $Log: pxp_entity_manager.ml,v $
+ * Revision 1.2  2002/10/22 14:22:54  gerd
+ * 	Corrected comment: pop_entity_until does NOT close the topmost
+ * entity.
+ *
  * Revision 1.1  2002/07/14 23:05:15  gerd
  * 	Initial revision.
  *
